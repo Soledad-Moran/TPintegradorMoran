ee838d4a83296dc86ebdde2807b1691c4739287e
Soledad Morán <124208872+Soledad-Moran@users.noreply.github.com>
2023-05-26 12:15:33 -0300

    Add files via upload

---
+++ JS/Java.js
@@ -0,0 +1,30 @@
+  /**
+Funcionalidad Javascript 
+para que al apretar “Resumen” 
+muestre el total a pagar
+ */
+
+  let precio = parseInt(document.getElementById("precio").innerText);
+  let btnBorrar = document.getElementById("borrar");
+  let btnResumen = document.getElementById("resumen");
+  
+  function resumen(precio, cantidad, categoria) {
+    let total = precio * cantidad;
+    let descuento = (total * categoria) / 100;
+    return total - descuento;
+  }
+  
+  btnResumen.addEventListener("click", () => {
+    let cantidad = parseInt(document.getElementById("cantidad").value);
+    let categoria = parseInt(document.getElementById("categoria").value);
+    document.getElementById("precioPagar").innerText = resumen(
+      precio,
+      cantidad,
+      categoria
+    );
+  });
+  
+  btnBorrar.addEventListener("click", () => {
+    document.getElementById("cantidad").value = 0;
+    document.getElementById("precioPagar").innerText = 0;
+  });
